4962d67e15f8becf427a8d6942558e0a7a3d12cf
jmpunk59 <jmpunk59@hotmail.fr>
2022-08-13 22:33:33 +0200

    Add Good computer + possibility to chose player mode

--- player.py
+++ player.py
@@ -1,4 +1,5 @@
 import random
+import math
 
 class Player():
 
@@ -35,4 +36,53 @@ class Computer(Player):
     def getMove(self,game):
         move = random.choice(game.available_spots())
         return move
+
+class GoodComputer(Player):
+
+    def __init__(self, letter):
+        super().__init__(letter)
     
+    def getMove(self, game):
+        if len(game.available_spots()) == 9:
+            spot = random.choice(game.available_spots())
+        else:
+            spot = self.minimax(game, self.letter)['position']
+        return spot
+    
+    def minimax(self, game_state, player):
+        max_player = self.letter
+        other_player = 'O' if player == 'X' else 'X'
+
+        #first, we want to check if the previous move is a winner
+        #this is our base case
+        if game_state.current_winner == other_player:
+            return  {'position': None, 
+                     'score': 1*(game_state.number_empty_spots() +1) if other_player == max_player else -1 * (game_state.number_empty_spots() + 1)}
+        
+        elif not game_state.empty_spots():
+            return {'position': None, 'score': 0}
+        
+        if player == max_player:
+            best = {'position': None, 'score': -math.inf}
+        else:
+            best = {'position': None, 'score': math.inf}
+        
+        for possible_spot in game_state.available_spots():
+            game_state.make_move(possible_spot, player)
+
+            sim_score = self.minimax(game_state, other_player)
+
+            game_state.board[possible_spot] = ' '
+            game_state.current_winner = None
+            sim_score['position'] = possible_spot
+
+            if player == max_player:
+                if sim_score['score'] > best['score']:
+                    best = sim_score
+            else:
+                if sim_score['score'] < best['score']:
+                    best = sim_score
+        return best
+
+
+
